9fc001fc02f6c89ff628d654e060e07ebe8e95fb
Oblin Guillaume <guillaume.oblin@sfr.fr>
2023-02-17 20:25:24 +0100

    creation de .gitignore; pour modele .js creation et validation des requete de categorie

--- modele.js
+++ modele.js
@@ -1,21 +1,83 @@
-// tri des film par note dans l'ordre decroissant
-const reponseBestScore = await fetch("http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score",
-                                    {method: "GET",
-                                    redirect: "follow"})
-                                    .then(response => response.json())
-                                    .catch(error => console.log('error', error)
-                                    );     
-// recuperation de la meilleur note                                                               
-const bestScore = JSON.stringify(reponseBestScore.results[0].imdb_score)
-const score = bestScore
-bestScore
+const url = "http://127.0.0.1:8000/api/v1/titles/"
 
-const reponse = await fetch("http://127.0.0.1:8000/api/v1/titles/?imdb_score=" + score + "&sort_by",
+// recuperation des 7 premieres notes par categories
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  
+let listElement = []
+let listallNotes = []
+    // boucle pour recuperer les note dans plusieurs page tans que listNote =7
+let numberPage = 0
+while (listElement.length < 7) {
+    numberPage++;
+    // Recuperation des element trier par imdb dans l'ordre decroissant(tous les films) 
+     const reponseBestScore = await fetch("http://127.0.0.1:8000/api/v1/titles/?genre=action&sort_by=-imdb_score", requestOptions)
+     .then(response => response.text())
+     .then(result => console.log(result))
+     .catch(error => console.log('error', error));
+    //ajout des element et notes des les arrays correspondant
+    for (const i in reponseBestScore.results) {
+        const element = reponseBestScore.results[i]
+        listElement.push(element)
+        //suppression  des doublons
+        if(!listallNotes.includes(parseFloat(element.imdb_score))){
+            listallNotes.push(parseFloat(element.imdb_score))
+        }
+    }
+}    
+
+
+
+/** 
+// recuperation des 7 premieres notes
+let listElement = []
+let listallNotes = []
+    // boucle pour recuperer les note dans plusieurs page tans que listNote =7
+let numberPage = 0
+while (listElement.length < 7) {
+    numberPage++;
+    // Recuperation des element trier par imdb dans l'ordre decroissant(tous les films) 
+     const reponseBestScore = await fetch (`${url}?page=${numberPage}&sort_by=-imdb_score`,
+                            {method: "GET",
+                            redirect: "follow"
+                            })
+                            .then(response => response.json())
+                            .catch(error => console.log('error', error));
+    //ajout des element et notes des les arrays correspondant
+    for (const i in reponseBestScore.results) {
+        const element = reponseBestScore.results[i]
+        listElement.push(element)
+        //suppression  des doublons
+        if(!listallNotes.includes(parseFloat(element.imdb_score))){
+            listallNotes.push(parseFloat(element.imdb_score))
+        }
+    }
+}    
+*/
+function sort(){
+    listElement.sort(function (a, b){
+        return b.votes - a.votes;
+    })
+    listElement.sort(function (a, b){
+        return b.imdb_score - a.imdb_score;
+    })
+}
+sort()
+const bestFilmId = await fetch (`${url}${listElement[0].id}`,
 {method: "GET",
-redirect: "follow"})
+redirect: "follow"
+})
 .then(response => response.json())
-.catch(error => console.log('error', error)
-);     
+.catch(error => console.log('error', error));
+
+
+
+
+console.log(bestFilmId)
+console.log(listElement)
 
 
 
@@ -29,6 +91,7 @@ class Film {
     
     constructor (elementRacine, options ={}, index) {
         this.element = elementRacine
+        this.id = options.id
         this.title = options.title
         this.urlImg = options.image_url
         this.descritpion = options.descritpion
@@ -36,7 +99,7 @@ class Film {
         // creation d'une balise pour le film
         const filmContainer = document.createElement("div")
         filmContainer.dataset.id = this.index
-        // creation d'une balise imge du film
+        // creation d'une balise image du film
         const imageFilm = document.createElement("img")
         imageFilm.src = this.urlImg
         // creation d'une balise tititre du film
@@ -59,9 +122,17 @@ class Film {
 }
 
 const racine = document.querySelector(".best_film")
-const objet = {
-    descritpion: "une petite description poiur la route",
-    title: "le tittre du film",
-    image_url: "IMG_0212.JPG"  }
+const objet = bestFilmId
 
 new Film(racine, objet, 1)
+const racineB = document.querySelector(".carousel_best_movies")
+for (let i = 0; i < 6; i++){
+  const theobjet = await fetch (`${url}${listElement[i].id}`,
+                             {method: "GET",
+                             redirect: "follow"
+                             })
+                            .then(response => response.json())
+                            .catch(error => console.log('error', error));
+   new Film(racineB, theobjet, i+1)                          
+}
+ 
