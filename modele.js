08119861d749ebed43a21389436a14a9470d4642
Oblin Guillaume <guillaume.oblin@sfr.fr>
2023-02-21 00:17:29 +0100

    validation class categorie

--- modele.js
+++ modele.js
@@ -1,5 +1,5 @@
 const url = "http://127.0.0.1:8000/api/v1/titles/"
-
+/** 
 // recuperation des 7 premieres notes par categories
 var requestOptions = {
     method: 'GET',
@@ -28,62 +28,10 @@ while (listElement.length < 7) {
         }
     }
 }    
-
-
-
-/** 
-// recuperation des 7 premieres notes
-let listElement = []
-let listallNotes = []
-    // boucle pour recuperer les note dans plusieurs page tans que listNote =7
-let numberPage = 0
-while (listElement.length < 7) {
-    numberPage++;
-    // Recuperation des element trier par imdb dans l'ordre decroissant(tous les films) 
-     const reponseBestScore = await fetch (`${url}?page=${numberPage}&sort_by=-imdb_score`,
-                            {method: "GET",
-                            redirect: "follow"
-                            })
-                            .then(response => response.json())
-                            .catch(error => console.log('error', error));
-    //ajout des element et notes des les arrays correspondant
-    for (const i in reponseBestScore.results) {
-        const element = reponseBestScore.results[i]
-        listElement.push(element)
-        //suppression  des doublons
-        if(!listallNotes.includes(parseFloat(element.imdb_score))){
-            listallNotes.push(parseFloat(element.imdb_score))
-        }
-    }
-}    
 */
-function sort(){
-    listElement.sort(function (a, b){
-        return b.votes - a.votes;
-    })
-    listElement.sort(function (a, b){
-        return b.imdb_score - a.imdb_score;
-    })
-}
-sort()
-const bestFilmId = await fetch (`${url}${listElement[0].id}`,
-{method: "GET",
-redirect: "follow"
-})
-.then(response => response.json())
-.catch(error => console.log('error', error));
 
 
-
-
-console.log(bestFilmId)
-console.log(listElement)
-
-
-
-
-
-class Film {
+const Film = class {
     /**
      * @param {element HTML} elementRacine 
      * @param {Objet} option 
@@ -96,6 +44,8 @@ class Film {
         this.urlImg = options.image_url
         this.descritpion = options.descritpion
         this.index = index
+    }
+    createHtmlElement () {
         // creation d'une balise pour le film
         const filmContainer = document.createElement("div")
         filmContainer.dataset.id = this.index
@@ -116,23 +66,60 @@ class Film {
         filmContainer.appendChild(titleFilm)
         filmContainer.appendChild(descriptionFilm)
         filmContainer.appendChild(filmBouton)
+    }    
+}
+
+const numberElementInCategories = 7
+const Categories = class {
+    /**
+     * 
+     * @param {element html} parentElement => emplacement de la categorie dans le html  
+     * @param {string} categoryName
+     */
+
+    constructor(parentElement, categoryName="") {
+        this.parent = document.querySelector(parentElement)
+        this.category=categoryName
+        this.idBestFilm = 
+        this.requestFilms()
     }
 
-    
+    /**
+     * récuperer une liste de 7 films tier dans l'ordre decroissant par score et votes de la categorie
+     * @returns {list of objet}
+     */
+    async requestFilms () {
+        const data = await fetch(`${url}?page_size=${numberElementInCategories}&genres=${this.category}&sort_by=-imdb_score,-votes`)
+        const jsonData = await data.json()
+        const filmElement = jsonData.results
+        this.createFilm(filmElement)
+        const idBestFilm = JSON.stringify(filmElement[0].id)
+        this.idBestFilm = idBestFilm
+    };
+
+    /**
+     * creation des instance dobjet Film pour chaque element de la liste.
+     * instance stoker dans this.filmObjectList.
+     */
+    createFilm (list) {
+        for (const element in list){
+            var film = new Film (this.parent, list[element], element)
+            film.createHtmlElement()
+        }
+    };
+
+    async bestFilm(){
+        const response = await fetch(`${url}${this.idBestFilm}`)
+        const jsonFilm = await response.json()
+        const film = jsonFilm.results
+        const racine = document.querySelector(".best_film")
+        new Film(racine, film, 1).createHtmlElement()
+    }
 }
 
-const racine = document.querySelector(".best_film")
-const objet = bestFilmId
+const bestMovies = new Categories(".carousel_best_movies", "")
+bestMovies.bestFilm()
+const toto = bestMovies.idBestFilm
+console.log(toto)
 
-new Film(racine, objet, 1)
-const racineB = document.querySelector(".carousel_best_movies")
-for (let i = 0; i < 6; i++){
-  const theobjet = await fetch (`${url}${listElement[i].id}`,
-                             {method: "GET",
-                             redirect: "follow"
-                             })
-                            .then(response => response.json())
-                            .catch(error => console.log('error', error));
-   new Film(racineB, theobjet, i+1)                          
-}
  
