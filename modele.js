443e90259ab863239a2b7cb56bfdf442f11d16f5
Guillaume <guillaume.oblin@greta-cfa-aquitaine.academy>
2023-03-01 15:58:52 +0100

    modification de modal- suppression des requete a chaque clic

--- modele.js
+++ modele.js
@@ -298,24 +298,24 @@ class Carousel {
 
     // modification du DOM
     this.carousel = this.object.carousel // let root grafikart
-    this.carousel.setAttribute('tabindex', '0',)
+    this.carousel.setAttribute('tabindex', '0')
     this.carouselContainer = this.object.carouselContainer // let container grafikart
     this.object.listFilms.then(element => this.setStyle(element))
     this.createNavigation()
 
     // Evenement
+    // eslint-disable-next-line n/no-callback-literal
     this.moveCallbacks.forEach(cb => cb(0))
-    this.onWindowResize ()
+    this.onWindowResize()
     window.addEventListener('resize', this.onWindowResize.bind(this))
     this.carousel.addEventListener('keyup', event => {
-        if(event.key === 'ArrowRight' || event.key === 'Right') {
-          this.next()
-        } else if (event.key == 'ArrowLeft' || event.key === 'Left') {
-          this.prev()
-        }
+      if (event.key === 'ArrowRight' || event.key === 'Right') {
+        this.next()
+      } else if (event.key === 'ArrowLeft' || event.key === 'Left') {
+        this.prev()
+      }
     })
-
-  }; 
+  };
 
   /**
    * Redimensions les elements en fonction de la taille de la fenetres
@@ -409,7 +409,7 @@ class Carousel {
   }
 
   onWindowResize () {
-    let mobile = window.innerWidth < 800
+    const mobile = window.innerWidth < 850
     if (mobile !== this.isMobile) {
       this.isMobile = mobile
       this.setStyle()
@@ -480,105 +480,11 @@ document.addEventListener('DOMContentLoaded', function () {
   })
 })
 
-
-/**
- * creation de la modal
- * @param {*} infoFilm 
- */
-
-
-function createHtmlElmentModal (infoFilm) {
-  debugger
-  const body = document.querySelector('body')
-  const modal = document.createElement('aside')
-  modal.className = 'modal'
-  modal.style.display = 'none'
-  const close = document.createElement('button')
-  close.className = 'close__modal'
-  close.innerText = 'Fermer'
-  // image du film
-
-  const imageFilm = document.createElement('img')
-  imageFilm.className = 'modal__film__img'
-  imageFilm.src = infoFilm.image_url
-
-  // tittre du film
-  const titleFilm = document.createElement('h2')
-  titleFilm.className = 'modal__film__title'
-  titleFilm.innerText = infoFilm.title
-
-  // info film
-  const listInfoFilm = document.createElement('ul')
-  listInfoFilm.className = 'info__film'
-
-  // genre complet du film
-  const genreFilm = document.createElement('li')
-  genreFilm.className = 'genre__film'
-  genreFilm.innerText = `Genre : ${infoFilm.genres}`
-
-  // date de sortie
-  const date = document.createElement('li')
-  date.className = 'date__film'
-  date.innerText = `Année de sortie : ${infoFilm.year}`
-
-  // son rated
-  const rated = document.createElement('li')
-  rated.className = 'rated__film'
-  rated.innerText = `Nombre évaluation : ${infoFilm.rated}`
-
-  // son score IMDB
-  const scoreImdb = document.createElement('li')
-  scoreImdb.className = 'score_film'
-  scoreImdb.innerText = `Note IMDB: ${infoFilm.imdb_score}`
-
-  // son realisateur
-  const real = document.createElement('li')
-  real.className = 'real__film'
-  real.innerText = `Réalisateur: ${infoFilm.directors}`
-
-  // liste des acteur
-  const actors = document.createElement('li')
-  actors.className = 'actors__film'
-  actors.innerText = `Acteurs: ${infoFilm.actors}`
-
-  // sa durée
-  const duration = document.createElement('li')
-  duration.innerText = `Durée: ${infoFilm.duration} minutes`
-
-  // pays d'origine
-  const country = document.createElement('li')
-  country.className = 'country__film'
-  country.innerText = `Pays: ${infoFilm.countries}`
-
-  // resultat box office
-  const boxOffice = document.createElement('li')
-  boxOffice.innerText = `Nombre entrée au Box Office: ${infoFilm.worldwide_gross_income}`
-
-  // resume du film
-  const resume = document.createElement('li')
-  resume.innerText = `Résumé: ${infoFilm.long_description}`
-
-  // rattachement au parent
-  body.appendChild(modal)
-  modal.appendChild(close)
-  modal.appendChild(imageFilm)
-  modal.appendChild(titleFilm)
-  modal.appendChild(listInfoFilm)
-  listInfoFilm.appendChild(genreFilm)
-  listInfoFilm.appendChild(date)
-  listInfoFilm.appendChild(rated)
-  listInfoFilm.appendChild(scoreImdb)
-  listInfoFilm.appendChild(real)
-  listInfoFilm.appendChild(actors)
-  listInfoFilm.appendChild(duration)
-  listInfoFilm.appendChild(country)
-  listInfoFilm.appendChild(boxOffice)
-  listInfoFilm.appendChild(resume)
-}
-
 let modal = null
 
 const target = document.addEventListener('click', function (event) {
+  const element = event.target.className
+  if (element !== 'item__image') return
   loadFilm(event.target)
 }
 )
@@ -590,8 +496,8 @@ function openModal () {
   target.setAttribute('aria-modal', 'true')
   modal = target
   modal.addEventListener('click', closeModal)
-  //modal.querySelector('close__modal').addEventListener('click', closeModal)
-  //modal.querySelector('modal__stop').addEventListener('click', stopPropagation)
+  // modal.querySelector('close__modal').addEventListener('click', closeModal)
+  // modal.querySelector('modal__stop').addEventListener('click', stopPropagation)
 }
 
 const closeModal = function (event) {
@@ -601,8 +507,8 @@ const closeModal = function (event) {
   modal.setAttribute('aria-hidden', 'true')
   modal.removeAttribute('aria-modal')
   modal.removeEventListener('click', closeModal)
-  //modal.querySelector('close__modal').removeEventListener('click', closeModal)
-  //modal.querySelector('modal__stop').removeEventListener('click', stopPropagation)
+  // modal.querySelector('close__modal').removeEventListener('click', closeModal)
+  // modal.querySelector('modal__stop').removeEventListener('click', stopPropagation)
   modal = null
   const aside = document.querySelector('aside')
   aside.remove()
